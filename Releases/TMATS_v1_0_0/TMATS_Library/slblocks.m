9e582268bd5f5cce97e42de306afd96b2c1b28ff
Jeff Chapman <Jeffryes.w.Chapman@nasa.gov>
2014-02-07 16:28:51 -0500

    Remove releases folder.
    Remove Releases folder, this is not needed because releases will now be
    under the releases tab of github.

--- Releases/TMATS_v1_0_0/TMATS_Library/slblocks.m
+++
@@ -1,6 +0,0 @@
-function blkStruct = slblocks
-  % Specify that the product should appear in the library browser
-  % and be cached in its repository
-  Browser.Library = 'TMATS_Lib';
-  Browser.Name    = 'T-MATS';
-  blkStruct.Browser = Browser;
