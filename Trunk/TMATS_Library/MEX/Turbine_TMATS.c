5a448ce12c836cf375d34860ae8301569d7d07ab
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2015-11-02 15:38:27 -0500

    Turbine Errors
    Added warnings to notify a user when the sizes of the map file
    definitions are wrong. Also added new outputs and combined the baseline
    turbine and the NPSS version. There were also some name changes for mask
    inputs (note, this will cause old inputs to be removed when using older
    models)

--- Trunk/TMATS_Library/MEX/Turbine_TMATS.c
+++ Trunk/TMATS_Library/MEX/Turbine_TMATS.c
@@ -29,7 +29,12 @@
 #define PRmapDes_p(S)           ssGetSFcnParam(S,14)
 #define NDes_p(S)               ssGetSFcnParam(S,15)
 #define BN_p(S)                 ssGetSFcnParam(S,16)
-#define NPARAMS 17
+#define WcMapCol_p(S)           ssGetSFcnParam(S,17)
+#define EffMapCol_p(S)          ssGetSFcnParam(S,18)
+#define WcMapRw_p(S)            ssGetSFcnParam(S,19)
+#define EffMapRw_p(S)           ssGetSFcnParam(S,20)
+#define ConfigNPSS_p(S)         ssGetSFcnParam(S,21)
+#define NPARAMS 22
 
 extern double h2tc(double a, double b);
 extern double pt2sc(double c, double d, double e);
@@ -47,32 +52,32 @@ static void mdlInitializeSizes(SimStruct *S)
         /* Return if number of expected != number of actual parameters */
         return;
     }
-
+    
     for (i = 0; i < NPARAMS; i++)
         ssSetSFcnParamTunable(S, i, 0);
-
+    
     ssSetNumContStates(S, 0);
     ssSetNumDiscStates(S, 0);
-
+    
     if (!ssSetNumInputPorts(S, 2)) return;
     ssSetInputPortWidth(S, 0, 8);
     ssSetInputPortRequiredContiguous(S, 0, true);
     ssSetInputPortDirectFeedThrough(S, 0, 1);
-
+    
     ssSetInputPortWidth(S, 1, DYNAMICALLY_SIZED);
     ssSetInputPortRequiredContiguous(S, 1, true);
     ssSetInputPortDirectFeedThrough(S, 1, 1);
-
+    
     if (!ssSetNumOutputPorts(S, 1)) return;
-    ssSetOutputPortWidth(S, 0, 12);
-
+    ssSetOutputPortWidth(S, 0, 20);
+    
     ssSetNumSampleTimes(S, 1);
     ssSetNumRWork(S, 0);
     ssSetNumIWork(S, 5);
     ssSetNumPWork(S, 0);
     ssSetNumModes(S, 0);
     ssSetNumNonsampledZCs(S, 0);
-
+    
 }
 
 #if defined(MATLAB_MEX_FILE)
@@ -124,21 +129,27 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     const real_T IDes               = *mxGetPr(IDesign_p(S));
     const int_T  BldPosLeng         = *mxGetPr(BldPosLeng_p(S));
     const int_T  CoolFlwEn          = *mxGetPr(CoolFlwEn_p(S));
-
+    const int_T  ConfigNPSS         = *mxGetPr(ConfigNPSS_p(S));
+    
     /* vector & array data */
     const real_T *Y_T_NcVec            = mxGetPr(Y_T_NcVec_p(S));
     const real_T *X_T_PRVec            = mxGetPr(X_T_PRVec_p(S));
     const real_T *T_T_Map_WcArray      = mxGetPr(T_T_Map_WcArray_p(S));
     const real_T *T_T_Map_EffArray     = mxGetPr(T_T_Map_EffArray_p(S));
     const real_T *T_BldPos             = mxGetPr(T_BldPos_p(S));
-
+    
     /*------get dimensions of parameter arrays-------*/
     const int_T A   = mxGetNumberOfElements(Y_T_NcVec_p(S));
     const int_T B   = mxGetNumberOfElements(X_T_PRVec_p(S));
-
+    
+    const int_T WcMapCol   = *mxGetPr(WcMapCol_p(S));
+    const int_T EffMapCol  = *mxGetPr(EffMapCol_p(S));
+    const int_T WcMapRw    = *mxGetPr(WcMapRw_p(S));
+    const int_T EffMapRw   = *mxGetPr(EffMapRw_p(S));
+    
     /*---------Define Inputs--------*/
     const real_T *u  = (const real_T*) ssGetInputPortSignal(S,0);
-
+    
     double WIn      = u[0];     /* Input Flow [pps]	*/
     double htIn     = u[1];     /* input enthalpy [BTU/lbm]*/
     double TtIn     = u[2];     /* Temperature Input [degR]  */
@@ -147,21 +158,21 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     double Nmech    = u[5];     /* Mechancial Shaft Speed [rpm]*/
     double PRIn     = u[6];     /* Pressure Ratio [NA] 	 */
     int    cfWidth  = u[7];     /* Cooling Flow vector length	*/
-
+    
     /*---------Define Inputs for input port 2--------*/
     /* N 5x1 vectors consisting of W, ht, Tt, Pt and FAR, where N is the number of cooling flows */
     const real_T *CoolFlow = ssGetInputPortRealSignal(S, 1);
-
+    
     real_T *y  = (real_T *)ssGetOutputPortRealSignal(S,0);   /* Output Array */
-
+    
     /*--------Define Constants-------*/
     double WOut, htOut, TtOut, PtOut, FARcOut, TorqueOut, NErrorOut;
-    double WcCalcin, WcMap, theta,delta, Pwrout, PRin, htin;
+    double WcCalcin, WcMap, theta,delta, Pwrout, PRin, htin, Wcin, Wcs1in;
     double TtIdealout, Test, htIdealout, Sout, NcMap, Nc, EffMap, Eff;
     double dHcools1, dHcoolout, Wfcools1, Wfcoolout, Ws1in,hts1in, Tts1in, Pts1in, FARs1in;
     double Ss1in, dHout, Wcoolout, Wcools1, PRmapRead;
     double C_Eff, C_PR, C_Nc, C_Wc;
-
+    
     int interpErr = 0;
     double Wcool[100];
     double htcool[100];
@@ -169,17 +180,17 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     double Ptcool[100];
     double FARcool[100];
     int Vtest, i;
-
+    
     /* ------- get strings -------------- */
     char * BlkNm;
     int_T buflen;
     int_T status;
-
+    
     /* Get name of block from dialog parameter (string) */
     buflen = mxGetN(BN_p(S))*sizeof(mxChar)+1;
     BlkNm = mxMalloc(buflen);
     status = mxGetString(BN_p(S), BlkNm, buflen);
-
+    
     /* Verify input bleed vector is a multiple of 5 */
     Vtest = cfWidth/5;
     if(5*Vtest != cfWidth && CoolFlwEn > 0.5 && ssGetIWork(S)[0]==0){
@@ -187,10 +198,10 @@ static void mdlOutputs(SimStruct *S, int_T tid)
         ssSetIWorkValue(S,0,1);
     }
     else if(BldPosLeng != cfWidth/5 && CoolFlwEn > 0.5 && ssGetIWork(S)[1]==0){
-        printf("Errorin %s, number of cooling flow inputs does not match the length of the Cooling flow postion vector in the mask\n",BlkNm);
+        printf("Error in %s, number of cooling flow inputs does not match the length of the Cooling flow postion vector in the mask\n",BlkNm);
         ssSetIWorkValue(S,1,1);
     }
-
+    
     /* unpack CoolFlow vector */
     for (i = 0; i < cfWidth/5; i++)
     {
@@ -209,16 +220,16 @@ static void mdlOutputs(SimStruct *S, int_T tid)
             htcool[i] = t2hc(Ttcool[i],FARcool[i]);
         }
     }
-
+    
     /* Initialize cooling flow sum constants */
-
+    
     dHcools1 = 0;   /* enthalpy * mass cooling flow rate at stage 1 of turbine */
     dHcoolout = 0;   /* enthalpy * mass cooling flow rate at exit of turbine */
     Wcools1 = 0;    /* total cooling flow at stage 1 of turbine*/
     Wcoolout = 0;    /* total cooling flow at output of turbine */
     Wfcools1 = 0;  /* combusted fuel flow in cooling at stage 1 of turbine */
     Wfcoolout = 0;  /* combusted fuel flow in cooling at exit of turbine */
-
+    
     /* calc cooling flow constants for stage 1 and output of the turbine */
     for (i = 0; i < cfWidth/5; i++)
     {
@@ -226,25 +237,25 @@ static void mdlOutputs(SimStruct *S, int_T tid)
             printf(" Error in %s, cooling flow postion element %i needs to be defined as a 0 or 1\n",BlkNm,i+1);
             ssSetIWorkValue(S,2,1);
         }
-
+        
         /* calc mass flow for cooling flows */
         Wcools1 = Wcools1 + Wcool[i]*(1-T_BldPos[i]);
         Wcoolout = Wcoolout + Wcool[i];
-
+        
         /* calc fuel mass flow for cooling flows*/
         Wfcools1 = Wfcools1 + FARcool[i]*Wcool[i]*(1-T_BldPos[i])/(1+FARcool[i]);
         Wfcoolout = Wfcoolout + FARcool[i]*Wcool[i]/(1+FARcool[i]);
     }
     /*-- Compute Total Flow  --------*/
-
+    
     Ws1in = WIn + Wcools1;  /* mass flow at station 1 */
     WOut = WIn + Wcoolout;   /* mass flow at turbine exit */
-
+    
     /*-- Compute Fuel to Air Ratios ---*/
-
+    
     FARs1in = (FARcIn* WIn/(1+FARcIn) + Wfcools1)/(WIn/(1+FARcIn) + Wcools1- Wfcools1);
     FARcOut = (FARcIn* WIn/(1+FARcIn)+ Wfcoolout)/(WIn/(1+FARcIn) + Wcoolout- Wfcoolout);
-
+    
     /* calc input enthalpy of cooling flow for stage 1 */
     for (i = 0; i < cfWidth/5; i++)
     {
@@ -253,60 +264,80 @@ static void mdlOutputs(SimStruct *S, int_T tid)
         /* Compute cooling flow dH for the exit of the turbine assuming input htcool = htcoolout for turbine rear bleeds  */
         dHcoolout = dHcoolout + htcool[i]*Wcool[i]*T_BldPos[i];
     }
-
+    
     /*-- Compute avg enthalpy at stage 1 --------*/
     htin = t2hc(TtIn,FARcIn);
     hts1in = (htin* WIn + dHcools1)/Ws1in;
-
+    
     /*-- Compute  stage 1 total temp--------*/
-
+    
     Tts1in = h2tc(hts1in,FARs1in);
-
+    
     /*-- Compute Stage 1 entropy, assuming PtIn = Pts1in  --------*/
     Ss1in = pt2sc(PtIn,Tts1in,FARs1in);
-
+    
     /*---- calculate misc. fluid condition related variables --------*/
     delta = PtIn / C_PSTD;
     theta = TtIn / C_TSTD;
-
+    
     /*------ Calculate corrected speed ---------*/
-    Nc = Nmech/sqrt(theta);
-
+    if (ConfigNPSS > 0.5)
+        Nc = Nmech/sqrt(theta*C_TSTD);
+    else
+        Nc = Nmech/sqrt(theta);
+    
+    
     if(IDes < 0.5)
         C_Nc = Nc / NcDes;
     else
         C_Nc = s_T_Nc;
-
+    
+    
     NcMap = Nc / C_Nc;
-
+    
     /*------ Compute pressure output --------*/
     if(IDes < 0.5)
         C_PR = (PRIn - 1)/(PRmapDes -1);
     else
         C_PR = s_T_PR;
-
+    
     PRmapRead = ((PRIn -1)/C_PR)+1;
-
+    
     PtOut = PtIn/PRIn;	/* using PR from input */
-
+    
     /*-- Compute Total Flow input (from Turbine map)  --------*/
-
+    
     WcMap = interp2Ac(X_T_PRVec,Y_T_NcVec,T_T_Map_WcArray,PRmapRead,NcMap,B,A,&interpErr);
-    if (interpErr == 1 && ssGetIWork(S)[3]==0){
+    if ((WcMapCol != B || WcMapRw != A) && ssGetIWork(S)[3]==0){
+        printf("Warning in %s, Error calculating WcMap. Table size does not match axis vector lengths.\n", BlkNm);
+        ssSetIWorkValue(S,3,1);
+    }
+    else if (interpErr == 1 && ssGetIWork(S)[3]==0){
         printf("Warning in %s, Error calculating WcMap. Vector definitions may need to be expanded.\n", BlkNm);
         ssSetIWorkValue(S,3,1);
     }
-    if(IDes < 0.5)
-        C_Wc = Ws1in*sqrt(theta)/delta / WcMap;
-    else
+    if(IDes < 0.5) {
+        if (ConfigNPSS > 0.5)
+            C_Wc = WIn*sqrt(theta)/delta / WcMap;
+        else
+            C_Wc = Ws1in*sqrt(theta)/delta / WcMap;
+    }
+    else {
         C_Wc = s_T_Wc;
-
+    }
+    
     WcCalcin = WcMap * C_Wc;
-
+    Wcin = WIn*sqrt(theta)/delta;
+    Wcs1in =  Ws1in*sqrt(theta)/delta;
+    
     /*-- Compute Turbine Efficiency (from Turbine map)  --------*/
-
+    
     EffMap = interp2Ac(X_T_PRVec,Y_T_NcVec,T_T_Map_EffArray,PRmapRead,NcMap,B,A,&interpErr);
-    if (interpErr == 1 && ssGetIWork(S)[4]==0){
+    if ((EffMapCol != B || EffMapRw != A) && ssGetIWork(S)[4]==0){
+        printf("Warning in %s, Error calculating EffMap. Table size does not match axis vector lengths.\n", BlkNm);
+        ssSetIWorkValue(S,4,1);
+    }
+    else if (interpErr == 1 && ssGetIWork(S)[4]==0){
         printf("Warning in %s, Error calculating EffMap. Vector definitions may need to be expanded.\n", BlkNm);
         ssSetIWorkValue(S,4,1);
     }
@@ -314,31 +345,31 @@ static void mdlOutputs(SimStruct *S, int_T tid)
         C_Eff = EffDes / EffMap;
     else
         C_Eff = s_T_Eff;
-
+    
     Eff = EffMap * C_Eff;
-
-
+    
+    
     /*------ enthalpy calculations ---------*/
     /* ---- Ideal enthalpy  ----*/
     Sout = Ss1in;
     TtIdealout = sp2tc(Sout,PtOut,FARcOut);
     htIdealout = t2hc(TtIdealout,FARcOut); /* may need to be updated due to TtIdealout not being correct */
-
+    
     /*-Compute power output only takes into account cooling flow that enters at front of engine (stage 1)-*/
-
+    
     Pwrout = ((hts1in - htIdealout)*Eff)*Ws1in * C_BTU_PER_SECtoHP;
-
+    
     /* ---- enthalpy output ----*/
-
+    
     htOut = ((((htIdealout - hts1in)*Eff) + hts1in)*Ws1in + dHcoolout)/WOut;
-
+    
     /*------ Compute Temperature output (empirical) ---------*/
-
+    
     TtOut = h2tc(htOut,FARcOut);
-
+    
     /*----- Compute output Torque to shaft ----*/
     TorqueOut = C_HP_PER_RPMtoFT_LBF * Pwrout/Nmech;
-
+    
     /* ----- Compute Normalized Flow Error ----- */
     if (IDes < 0.5 && NDes == 0)
         NErrorOut = 100;
@@ -348,9 +379,16 @@ static void mdlOutputs(SimStruct *S, int_T tid)
         NErrorOut = 100;
     }
     else {
-        NErrorOut = (Ws1in*sqrt(theta)/delta-WcCalcin)/(Ws1in*sqrt(theta)/delta) ;
+        if (ConfigNPSS > 0.5){
+            /* map contains turbine input flow only */
+            NErrorOut = (Wcin-WcCalcin)/Wcin ;
+        }
+        else {
+            /* map contains turbine input flow and input bleed flow */
+            NErrorOut = (Wcs1in-WcCalcin)/Wcs1in ;
+        }
     }
-    Test = Wcool[0];
+    Test = PRmapRead;
     /*------Assign output values------------        */
     y[0] = WOut;            /* Outlet Total Flow [pps]   */
     y[1] = htOut;           /* Outlet Enthalpy [BTU/lbm]*/
@@ -363,8 +401,16 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     y[8] = C_Wc;            /* Corrected Flow Scalar */
     y[9] = C_PR;            /* Pressure Ratio Scalar */
     y[10] = C_Eff;          /* Efficiency Scalar */
-    y[11] = Test;
-
+    y[11] = Wcin;           /* Corrected input flow [pps] */
+    y[12] = Wcs1in;         /* Station 1 corrected input flow [pps] */
+    y[13] = Nc;             /* Corrected speed [rpm]*/
+    y[14] = NcMap;          /* Map corrected speed */
+    y[15] = WcMap;          /* Map corrected flow */
+    y[16] = PRmapRead;      /* Map pressure ratio */
+    y[17] = EffMap;         /* Map efficiency */
+    y[18] = Pwrout;         /* Output power [hp]*/
+    y[19] = Test;
+    
 }
 
 static void mdlTerminate(SimStruct *S)
