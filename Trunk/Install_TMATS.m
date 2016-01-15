35056a7a3b3c0c21918d4df6ecafb292c6b76d6b
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2016-01-15 13:06:40 -0500

    Ver number to v 1.1.2d
    update to v1.1.2d for development. This helps to know what version the
    user is using.

--- Trunk/Install_TMATS.m
+++ Trunk/Install_TMATS.m
@@ -3,7 +3,7 @@ function Install_TMATS()
 
 % TMATSver and date must remain in the listed form for the ver command to work
 % properly.
-TMATSver = '1.1.2';
+TMATSver = '1.1.2d';
 TMATSdate = '15-Jan-2016';
 
 error = 0;
