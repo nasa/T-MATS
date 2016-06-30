cfb026571fb608e316902f789f51b9f438c1613e
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2016-06-30 08:41:17 -0400

    Correct compressor mask txt, v1.1.3.1
    corrected FBLDEN_M label to Fractional Bleed enable, from customer bleed
    enable.

--- Trunk/Install_TMATS.m
+++ Trunk/Install_TMATS.m
@@ -3,8 +3,8 @@ function Install_TMATS()
 
 % TMATSver and date must remain in the listed form for the ver command to work
 % properly.
-TMATSver = '1.1.3';
-TMATSdate = '19-May-2016';
+TMATSver = '1.1.3.1';
+TMATSdate = '30-Jun-2016';
 
 error = 0;
 TMATSInstallmsg = 'Install T-MATS matlab toolbox? Note: Installation will add MATLAB paths and generate MEX files.';
