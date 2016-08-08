318646feaa2578588d3e1ce8e1da90f22cca8560
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2016-08-08 11:17:58 -0400

    Heat Soak block v1.1.3.8
    Created heat soak block. This block models the effects of a material
    (lump mass) on the fluid stream. At a high level the block caluclates
    the convective heat transfer between the two mediums, adjusts the energy
    of the fluid stream to find the heat soaked temperature, then determines
    the change in metal temperature for a dTm/dt to be used to update Tm
    (see block help for more information on the subject).

--- Trunk/Install_TMATS.m
+++ Trunk/Install_TMATS.m
@@ -3,8 +3,8 @@ function Install_TMATS()
 
 % TMATSver and date must remain in the listed form for the ver command to work
 % properly.
-TMATSver = '1.1.3.7';
-TMATSdate = '05Aug-2016';
+TMATSver = '1.1.3.8';
+TMATSdate = '08Aug-2016';
 
 error = 0;
 TMATSInstallmsg = 'Install T-MATS matlab toolbox? Note: Installation will add MATLAB paths and generate MEX files.';
