93f44d8b839b3a93462dea09d67f06bc8eea34c4
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2016-08-11 15:48:52 -0400

    1-D heat Xfer block for all cases v1.1.3.12
    Posted 1-D heat Xfer block as a replacement for all previous 1-D heat
    Xfer blocks. This block has it all, and is highly adjustable. Boundary
    Convection, Conduction, and Radiation are all available. Node sizes and
    Conduction coefficients are adjustable as a function of distance.
    Material contants can all be adjusted dynamically. This block also has
    the options of planar or cylindrical coordinates.

--- Trunk/Install_TMATS.m
+++ Trunk/Install_TMATS.m
@@ -3,7 +3,7 @@ function Install_TMATS()
 
 % TMATSver and date must remain in the listed form for the ver command to work
 % properly.
-TMATSver = '1.1.3.11';
+TMATSver = '1.1.3.12';
 TMATSdate = '11Aug-2016';
 
 error = 0;
