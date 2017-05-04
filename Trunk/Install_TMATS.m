c18a825f0c7bbf8e78d103fcc013ec7c43fd0dbc
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2017-05-04 14:46:36 -0400

    4D PW state-space model v1.1.3.30
    Add a piecewise state-space model. This will allow up to 4 dimensions. The definitions may be added manually, or can be linked to a workspace structure. This structure may be generated with the T-MATS PW linear model generator block.

--- Trunk/Install_TMATS.m
+++ Trunk/Install_TMATS.m
@@ -3,8 +3,8 @@ function Install_TMATS()
 
 % TMATSver and date must remain in the listed form for the ver command to work
 % properly.
-TMATSver = '1.1.3.29';
-TMATSdate = '28Apr-2017';
+TMATSver = '1.1.3.30';
+TMATSdate = 'MayThe4thBeWithYou-2017';
 
 error = 0;
 TMATSInstallmsg = 'Install T-MATS matlab toolbox? Note: Installation will add MATLAB paths and generate MEX files.';
