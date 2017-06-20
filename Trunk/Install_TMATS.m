8dddc0f7e3b58f4ef92f315163e0778e667da6fd
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2017-06-20 15:02:05 -0400

    Turbojet IC v1.1.2.34
    Shaft speed sensor in Dynamic Turbojet model was not set correctly.  Updated to be set to initial condition.

--- Trunk/Install_TMATS.m
+++ Trunk/Install_TMATS.m
@@ -3,8 +3,8 @@ function Install_TMATS()
 
 % TMATSver and date must remain in the listed form for the ver command to work
 % properly.
-TMATSver = '1.1.3.33';
-TMATSdate = 'Jun-14-2017';
+TMATSver = '1.1.3.34';
+TMATSdate = 'Jun-20-2017';
 
 error = 0;
 TMATSInstallmsg = 'Install T-MATS matlab toolbox? Note: Installation will add MATLAB paths and generate MEX files.';
