67bf5f560fab0805f89dd0cd5fa85691e8340a57
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2017-09-28 10:18:02 -0400

    BPR is bypass ratio v1.1.3.43
    Updated naming in the splitter and mixer to specifiy BPR is Bypass ratio

--- Trunk/Install_TMATS.m
+++ Trunk/Install_TMATS.m
@@ -3,8 +3,8 @@ function Install_TMATS()
 
 % TMATSver and date must remain in the listed form for the ver command to work
 % properly.
-TMATSver = '1.1.3.42';
-TMATSdate = 'Sept-26-2017';
+TMATSver = '1.1.3.43';
+TMATSdate = 'Sept-28-2017';
 
 error = 0;
 TMATSInstallmsg = 'Install T-MATS matlab toolbox? Note: Installation will add MATLAB paths and generate MEX files.';
