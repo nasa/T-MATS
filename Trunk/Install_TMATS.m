ae13d3e1a05a83284783a9a01d059546012e0dad
Jeffryes W Chapman <jeffryes.w.chapman@nasa.gov>
2017-11-30 09:44:40 -0500

    Documentation v1.2
    Update to version 1.2

--- Trunk/Install_TMATS.m
+++ Trunk/Install_TMATS.m
@@ -3,8 +3,8 @@ function Install_TMATS()
 
 % TMATSver and date must remain in the listed form for the ver command to work
 % properly.
-TMATSver = '1.1.3.46';
-TMATSdate = 'Nov-08-2017';
+TMATSver = '1.2.0';
+TMATSdate = 'Nov-30-2017';
 
 error = 0;
 TMATSInstallmsg = 'Install T-MATS matlab toolbox? Note: Installation will add MATLAB paths and generate MEX files.';
